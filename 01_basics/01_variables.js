5ba2276a04dcf1026e257a1af32e7fa9b6417bae
rahuldeore2023 <134832322+rahuldeore2023@users.noreply.github.com>
2024-10-07 12:56:57 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "rahul@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+//accountId = 2 
+
+accountEmail = "hc@g.com"
+accountPassword = "22000"
+accountCity = "Banglore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //We are using nodejs not browser
+
+// console.log( 3 + 3)
+// console.log("Rahul")
+
+let name = "hitesh"
+let age = 25
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null);  //object
+console.log(typeof undefined); // undefined
