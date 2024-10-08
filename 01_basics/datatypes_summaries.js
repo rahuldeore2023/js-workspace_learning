7d82fa79268c4734d626b3cc43cc49873574ee7c
rahuldeore2023 <134832322+rahuldeore2023@users.noreply.github.com>
2024-10-08 12:03:06 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summaries.js
@@ -0,0 +1,26 @@
+// Premitive
+// 7 types : 
+// 1.String, 2.Number, 3.Boolean, 4.null, 5.undefined, 6.Symbol, 
+// 7.BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //Result => false
+
+const bigNumber = 3456543576654356754n  
+
+// Reference (Non Premitive)
+
+//1.Array, 2.Objects, 3.Functions
+
+const heros = ["ASSS","NNN","MMMM"] 
