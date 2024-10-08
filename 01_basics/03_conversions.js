0fead1b1d2a029950bbf3fbacf09254345a14e4f
rahuldeore2023 <134832322+rahuldeore2023@users.noreply.github.com>
2024-10-08 05:48:30 +0000

    new conversions added

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,68 @@
+// let score = "Rahul"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// //console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"  => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "Rahul";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// 1 => true;  0 => false;
+
+//"" => false
+
+// "Rahul" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+let str1 = "hello";
+let str2 = " Rahul";
+
+let str3 = str1+str2
+// console.log(str3)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100;
+++gameCounter;
+//console.log(gameCounter);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log("---------------");
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//===
+console.log("2" === 2);
+
